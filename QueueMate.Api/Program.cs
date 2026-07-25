7f1a0d28f8c916d854527084a782e424fed4f96e
Aaditya Parab <parabaaditya785@student.sfit.ac.in>
2026-07-25 05:47:08 +0530

    Auth process

--- QueueMate.Api/Program.cs
+++ QueueMate.Api/Program.cs
@@ -1,9 +1,10 @@
-using Microsoft.EntityFrameworkCore;
-using QueueMate.Api.Data;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using QueueMate.Api.Data;
 using QueueMate.Api.Helpers;
 using QueueMate.Api.Interfaces;
 using QueueMate.Api.Models;
@@ -19,7 +20,7 @@ var connectionString =
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
-    
+
 builder.Services.Configure<JwtSettings>(
     builder.Configuration.GetSection(JwtSettings.SectionName));
 
@@ -62,11 +63,44 @@ builder.Services
             };
     });
 
-builder.Services.AddAuthorization();    
+builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "QueueMate API",
+        Version = "v1"
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter your JWT token."
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 builder.Services.AddCors(options =>
 {
@@ -91,6 +125,7 @@ app.UseHttpsRedirection();
 
 app.UseCors("Frontend");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- QueueMate.Api/obj/Debug/net8.0/QueueMate.Api.AssemblyInfo.cs
+++ QueueMate.Api/obj/Debug/net8.0/QueueMate.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QueueMate.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a4d0f7c3fbd1a25ed942afa15e8bac4355bbd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a17a7b7c789ba4224b62b6fe7e9ff59b43dd7d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("QueueMate.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QueueMate.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
